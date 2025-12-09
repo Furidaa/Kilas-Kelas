d37f2ec172de14634551106fb5deca3fbaf3fd37
Furidaa <kepodahli4@gmail.com>
2025-12-09 18:00:37 +0700

    Update renderGeneral.js

--- js/renderGeneral.js
+++ js/renderGeneral.js
@@ -1,15 +1,15 @@
 (async function () {
   const data = await loadKilasData();
-  const wrap = document.getElementById("generalInfo");
+  const list = document.getElementById("generalInfo");
 
   data.info_umum.forEach((info) => {
     const card = document.createElement("div");
     card.className = "card";
     card.innerHTML = `
       <p><strong>${info.judul}</strong></p>
-      <p class="small-text">Update: ${info.tanggal_info}</p>
-      <p style="margin-top:0.4rem;">${info.isi}</p>
+      <p class="small-text">${info.tanggal_info}</p>
+      <p class="small-text">${info.isi}</p>
     `;
-    wrap.appendChild(card);
+    list.appendChild(card);
   });
 })();
